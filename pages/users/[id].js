5321d19c6bad7159686ed4a7e17a1f3d18b9e1c9
Bob <bob_w@qq.com>
2021-01-11 15:14:07 +0800

    动态路由获取参数

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,10 +1,11 @@
 import React from 'react';
 import styles from './[id].css';
 
-export default () => {
+export default (props) => {
   return (
     <div>
       <h1 className={styles.title}>Page User</h1>
+      <p>{props.match.params.id}</p>
     </div>
   );
 }
