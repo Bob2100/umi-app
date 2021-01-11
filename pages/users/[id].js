795cf4d12a102a0be3447570531a1e5e703c4a22
Bob <bob_w@qq.com>
2021-01-11 15:59:48 +0800

    使用umi link

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './[id].css';
 
 export default (props) => {
--- pages/users/_layout.js
+++ pages/users/_layout.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styles from './_layout.css';
 
 export default (props) => {
