0177bd2ab588605fe89ce35e239bece8faa37b78
Bob <bob_w@qq.com>
2021-01-11 15:39:14 +0800

    展示用户列表

--- pages/users/index.js
+++ pages/users/index.js
@@ -2,9 +2,18 @@ import React from 'react';
 import styles from './index.css';
 
 export default () => {
+  const users = [
+    { id: 1, name: 'Bob' },
+    { id: 2, name: 'Jack' },
+    { id: 3, name: 'Rose' }
+  ];
+
   return (
     <div>
       <h1 className={styles.title}>Page users/index</h1>
+      <ul>
+        {users.map((user) => (<li key={user.id}>{user.id + ' - ' + user.name}</li>))}
+      </ul>
     </div>
   );
 }
